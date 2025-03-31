0119a7bf1ece35e81ae83174da15e7fee9b66f76
SETAVR <82760597+SETAVR@users.noreply.github.com>
2025-03-31 21:08:31 +0300

    Update main.cpp

--- 2lab/2(7)/main.cpp
+++ 2lab/2(7)/main.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include "Rectangle.h"
+using namespace std;
+int main() {
+    int x1, y1, x2, y2;
+
+    cout << "Введите x1, y1, x2, y2 через пробел: ";
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    Rectangle rect(x1, y1, x2, y2);
+
+    cout << rect.getInfo() << endl;
+
+    return 0;
+}
