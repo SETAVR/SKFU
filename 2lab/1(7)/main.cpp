0e6fe41281a903d3e16d3ab932be48da45032c96
SETAVR <82760597+SETAVR@users.noreply.github.com>
2025-03-31 20:22:52 +0300

    Update main.cpp

--- 2lab/1(7)/main.cpp
+++ 2lab/1(7)/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "TimeConverter.h"
+using namespace std;
 
+int main() {
+    int hours, minutes;
+
+    cout << "Введите количество часов: ";
+    cin >> hours;
+    cout << "Введите количество минут: ";
+    cin >> minutes;
+
+    TimeConverter timeee(hours, minutes);
+
+    cout << timeee.getInfo() << endl;
+    cout << "Общее количество минут: " << timeee.Minutes() << endl;
+
+    return 0;
+}
