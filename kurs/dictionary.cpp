77266d0c714cdedb59b72a905ff0b9a2611f400d
SETAVR <82760597+SETAVR@users.noreply.github.com>
2025-06-19 21:59:22 +0300

    Update dictionary.cpp

--- kurs/dictionary.cpp
+++ kurs/dictionary.cpp
@@ -1,49 +1,60 @@
-#include "dictionary.h"
-#include <fstream>
-#include <iostream>
-#include <locale>
+#include "dictionary.h" // Подключаем наш заголовочный файл
+#include <fstream>  // Для работы с файлами
+#include <iostream> // Для ввода/вывода
+#include <locale>   // Для поддержки русского языка
 
-using namespace std;
+using namespace std; // Чтобы не писать std::
 
+// Добавляет пару слов в оба словаря
 void Dictionary::addPair(const string& eng, const string& rus) {
-    engToRus[eng].push_back(rus);
-    rusToEng[rus].push_back(eng);
+    addPairImpl(eng, rus); // Вызов protected-метода базового класса
 }
 
+// Загружает словарь из файла
 bool Dictionary::loadFromFile(const string& filename) {
+    // Очищаем старые данные
     engToRus.clear();
     rusToEng.clear();
 
+    // Пытаемся открыть файл
     ifstream file(filename);
     if (!file.is_open()) {
         cout << "Ошибка: файл не найден!\n";
         return false;
     }
 
+    // Читаем файл построчно
     string line;
     while (getline(file, line)) {
+        // Ищем разделитель между словами
         size_t delimiter = line.find(':');
         if (delimiter != string::npos) {
+            // Разделяем строку на английское и русское слово
             string eng = line.substr(0, delimiter);
             string rus = line.substr(delimiter + 1);
+
+            // Добавляем пару в словарь
             addPair(eng, rus);
         }
     }
 
-    file.close();
+    file.close(); // Закрываем файл
     cout << "Словарь успешно загружен!\n";
     return true;
 }
 
+// Ищет перевод английского слова
 vector<string> Dictionary::translateEng(const string& word) const {
+    // Пытаемся найти слово в словаре
     auto it = engToRus.find(word);
     if (it == engToRus.end()) {
         cout << "Перевод не найден!\n";
-        return {};
+        return {}; // Возвращаем пустой список
     }
-    return it->second;
+    return it->second; // Возвращаем все варианты перевода
 }
 
+// Ищет перевод русского слова (работает аналогично)
 vector<string> Dictionary::translateRus(const string& word) const {
     auto it = rusToEng.find(word);
     if (it == rusToEng.end()) {
@@ -53,6 +64,7 @@ vector<string> Dictionary::translateRus(const string& word) const {
     return it->second;
 }
 
+// Показывает меню пользователю
 void DictionaryUI::showMenu() const {
     cout << "\n=== Англо-русский словарь ===\n";
     cout << "1. Загрузить словарь\n";
@@ -62,16 +74,17 @@ void DictionaryUI::showMenu() const {
     cout << "Выберите действие: ";
 }
 
+// Обрабатывает выбор пользователя
 void DictionaryUI::processChoice(int choice) {
     switch (choice) {
-    case 1: {
+    case 1: { // Загрузка словаря
         string filename;
         cout << "Введите имя файла: ";
         cin >> filename;
         dictionaryLoaded = dictionary.loadFromFile(filename);
         break;
     }
-    case 2: {
+    case 2: { // Англо-русский перевод
         if (!dictionaryLoaded) {
             cout << "Сначала загрузите словарь!\n";
             break;
@@ -89,7 +102,7 @@ void DictionaryUI::processChoice(int choice) {
         }
         break;
     }
-    case 3: {
+    case 3: { // Русско-английский перевод (аналогично)
         if (!dictionaryLoaded) {
             cout << "Сначала загрузите словарь!\n";
             break;
@@ -107,21 +120,25 @@ void DictionaryUI::processChoice(int choice) {
         }
         break;
     }
-    case 4:
-        exit(0);
-    default:
+    case 4: // Выход
+        exit(0); // Завершение программы
+    default: // Неправильный ввод
         cout << "Неверный выбор!\n";
     }
 }
 
+// Главный цикл программы
 void DictionaryUI::run() {
+    // Настраиваем консоль для русского языка
     setlocale(LC_ALL, "Russian");
-    system("chcp 1251 > nul");
+    system("chcp 1251 > nul"); // Устанавливаем кодировку
 
+    // Бесконечный цикл работы программы
     while (true) {
-        showMenu();
+        showMenu(); // Показываем меню
         int choice;
-        cin >> choice;
-        processChoice(choice);
+        cin >> choice; // Читаем выбор пользователя
+        processChoice(choice); // Выполняем выбранное действие
     }
 }
+
